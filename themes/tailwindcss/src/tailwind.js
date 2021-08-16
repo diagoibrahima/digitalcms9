1e9d5892f109afaa1894525c32dc130769765b74
Doudou Mohamet GAYE <doudoumohametgaye@gmail.com>
2021-08-16 16:05:40 +0000

    view single sub module ok

--- themes/tailwindcss/src/tailwind.js
+++ themes/tailwindcss/src/tailwind.js
@@ -1,91 +1,27 @@
-
-//var number = parseInt(jQuery('.numberofmodule').text());
-//number==0 || number==1 ?  jQuery('.numberofmodule').append('module'):jQuery('.numberofmodule').append('modules')
-//var numberofmodule = jQuery('.views-label-field-modulelibele').text('Nbmodule').length;
-//console.log(numberofmodule);
-//jQuery('.item-list').append(numberofmodule+"nombre de module");
 jQuery(document).ready(function() {
 
+  jQuery('.blockmodule-submodule').hide();
 
-  //jQuery('<div class="courer">').insertBefore('.listes-courses > a');
+
  
- // jQuery('</div>').insertAfter('.item-list');
+  jQuery('.module-section').click(function(){
 
 
- //jQuery('.listes-courses > a').prependTo('.item-list');
+  var moduleIndex = jQuery(this).index();
+  console.log(moduleIndex);
+  jQuery('.field-content:eq('+moduleIndex+') .blockmodule-submodule').toggle('fast');
 
-    
-
-    
-
-        
-
-       
+  jQuery('.field-content:eq('+moduleIndex+') .fa-chevron-down').toggleClass( 'ch-rotation' );
+  jQuery('.field-content:eq('+moduleIndex+') .blockmodule-titre-module').addClass('blockmodule-titre-module-green');
 
 
-
- //jQuery('#edit-submit').val('Create');
- jQuery('.js-form-item.form-item.js-form-type-textfield.form-item-title-0-value.js-form-item-title-0-value label').html('Label');
-
-    jQuery('.item-list').each(function()
-    {
-      var totmarks=0;
-
-    //  console.log(this); 
-
-        jQuery(this).find('.views-label').each(function()
-        {
-     
-
-      var marks = jQuery(this).text();
-    
-       
-          
-           if (marks.length!==0)
-            {
-              totmarks+=parseFloat(marks);
-              //console.log(totmarks);
-             
-            }
-        });
-        console.log(totmarks);
-       
-        if(totmarks == 0 || totmarks == 1){
-
-           jQuery(this).find('ul').html(totmarks  +' Module');
-        }
-       else
-       jQuery(this).find('ul').html(totmarks  +' Modules');
-       
-    });
-
-   
-
-
-
-
-
-
-
-
-
-        jQuery('.blockmodule-des>h3').append('<i class="expand-minimize-button fas text-light text-xl font-thin text-gray-400 fa-chevron-down"></i>');
-        jQuery('.blockmodule-des>h3 , .fa-chevron-down , .expandall-minimizeall-button').click(function() {
-        jQuery('.blockmodule-des').addClass('blockmodule-des-open');
-        jQuery('.contextual').addClass('blockmodule-des-modale');
-    });
-
-    jQuery('.blockmodule-des>h3 , .fa-chevron-down ').click(function() {
-    jQuery('.blockmodule-des>div').toggle();
-    jQuery('h3').toggleClass( 'colore-black' ); 
-    jQuery('.fa-chevron-down').toggleClass( 'ch-rotation' );
-
-    });
+});
 
 
     jQuery('.expandall-minimizeall-button').click(function() {
-    jQuery('.blockmodule-des>div').toggle();
+    jQuery('.blockmodule-submodule').toggle();
     jQuery('h3').toggleClass( 'colore-black' ); 
+    jQuery('.blockmodule-titre-module').addClass('blockmodule-titre-module-green');
     jQuery('.fa-chevron-down').toggleClass( 'ch-rotation' );
     var expand = (jQuery('.expandall-minimizeall-button').text());
     expand == "Expand all" ?  jQuery('.expandall-minimizeall-button').text('Minimize') : jQuery('.expandall-minimizeall-button').text('Expand all')
