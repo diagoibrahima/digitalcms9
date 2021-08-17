b2602ad6d9c28bfcb7a1343b6be41e5c2b99dc1f
Doudou Mohamet GAYE <doudoumohametgaye@gmail.com>
2021-08-17 13:23:32 +0000

    add new course on the course page ok

--- themes/tailwindcss/src/tailwind.js
+++ themes/tailwindcss/src/tailwind.js
@@ -4,24 +4,17 @@ jQuery(document).ready(function() {
 
   jQuery('.views-view-grid.horizontal.cols-4.clearfix .views-row').last().append('<div class="views-col col-1 addcourse-blank" style="width: 25%;"><div class="views-field views-field-nothing"><span class="field-content"><div class=" tw-item-card "><a href="node/add/course_" class="addcoursebutton" tabindex="-1">New Course</a></div></div> ');
 
-
- 
+  // Expand switch for one single learning module 
   jQuery('.module-section').click(function(){
-
-
   var moduleIndex = jQuery(this).index();
-  console.log(moduleIndex);
   jQuery('.field-content:eq('+moduleIndex+') .blockmodule-submodule').toggle('fast');
-
   jQuery('.field-content:eq('+moduleIndex+') .fa-chevron-down').toggleClass( 'ch-rotation' );
   jQuery('.field-content:eq('+moduleIndex+') .blockmodule-titre-module').addClass('blockmodule-titre-module-green');
+  });
 
-
-});
-
-
+      // Expand All 
     jQuery('.expandall-minimizeall-button').click(function() {
-    jQuery('.blockmodule-submodule').toggle();
+    jQuery('.blockmodule-submodule').toggle('fast');
     jQuery('h3').toggleClass( 'colore-black' ); 
     jQuery('.blockmodule-titre-module').addClass('blockmodule-titre-module-green');
     jQuery('.fa-chevron-down').toggleClass( 'ch-rotation' );
