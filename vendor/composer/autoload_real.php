d3d3974aa1f9b9662e5dcd396d5f5a84c9976160
Ibrahima <elibrahimadiago@gmail.com>
2021-08-24 14:13:01 +0000

    dump autoload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit9de9436f91c599173609d35a420479fb
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit9de9436f91c599173609d35a420479fb', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit9de9436f91c599173609d35a420479fb', 'loadClassLoader'));
