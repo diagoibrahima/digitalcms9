ec38a2a293a042e87e87639ee7917379a32a51cc
Ibrahima <elibrahimadiago@gmail.com>
2021-08-25 13:13:16 +0000

    add cre

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -796,7 +796,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
 $databases['default']['default'] = array (
-  'database' => 'cmstranslation',
+  'database' => 'dev_drupal_lms',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
