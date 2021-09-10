dd907a1829de43930f054f8d3bee5ad3f56ee442
Doudou Mohamet GAYE <doudoumohametgaye@gmail.com>
2021-09-10 12:36:24 +0000

    content completion ok

--- themes/tailwindcss/src/tailwind.js
+++ themes/tailwindcss/src/tailwind.js
@@ -4,8 +4,6 @@ jQuery(document).ready(function() {
  jQuery('.add-module , .use-ajax').hide();
  jQuery('option[value="_none"]').remove();
 
- 
-
 
 //content completion 
 //nombre de message traduit
@@ -18,10 +16,19 @@ var restotranslate=totalmessage-nombredemessagetraduit;
 var pourcentage = nombredemessagetraduit*100/totalmessage;
 var pourcentagemyInt = parseInt(pourcentage);
 
-console.log(restotranslate);
+
 jQuery('.contentrestetotranslate').html(restotranslate +' '+'to translate');
 //jQuery('div#block-views-block-completionnbmessage-block-1 header').append('<h1> voici le pourcentage '+pourcentagemyInt +'<h1>')
 jQuery('p.tw-font-bold.tw-text-5xl.tw-text-center.tw-my-3.tw-text-red-600.tw-translations-indicator').html(pourcentagemyInt+'%')
+if(pourcentagemyInt >=33){
+  jQuery('p.tw-font-bold.tw-text-5xl.tw-text-center.tw-my-3.tw-text-red-600.tw-translations-indicator').addClass('colo-midle');
+  jQuery('p.tw-rounded.tw-bg-red-100.tw-border.tw-border-red-500.tw-text-gray-800.p-1').addClass('coloremidel');
+}
+if(pourcentagemyInt >=100){
+  jQuery('p.tw-font-bold.tw-text-5xl.tw-text-center.tw-my-3.tw-text-red-600.tw-translations-indicator').addClass('colo-midlegr');
+  jQuery('p.tw-rounded.tw-bg-red-100.tw-border.tw-border-red-500.tw-text-gray-800.p-1').addClass('coloremidelgr');
+  jQuery('p.tw-rounded.tw-bg-red-100.tw-border.tw-border-red-500.tw-text-gray-800.p-1').html('This course has been fully translated');
+}
 
 
 // jQuery('.a.use-ajax').hide();
