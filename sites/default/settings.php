7601ebcc499d7755e25c84cd2574c0ff33f87c4b
dmgaye96 <doudoumohametgaye@gmail.com>
2022-01-31 14:08:19 +0000

    update 31 janvier

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -795,6 +795,11 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
+<<<<<<< HEAD
+=======
+
+ //BD en 
+>>>>>>> 64c4d9193 (login destination)
 /*
 $databases['default']['default'] = array (
   'database' => 'dev_drupal_lms',
