f2ccfc9f899e734136d5fe7688281e2ab0651d94
dmgaye96 <doudoumohametgaye@gmail.com>
2022-02-07 13:10:15 +0000

    update top barr and show card margin

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,11 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-<<<<<<< HEAD
-        'reference' => '1f1c7a82d7560bfae581913c2128e90875c2ca5a',
-=======
-        'reference' => '719368494c89a904417b284254a6799927eb685d',
->>>>>>> 6347b75e5 (updat edite mode)
+        'reference' => 'cc89b094e6a9e40fcd253c68a337264e2cbc0b78',
         'name' => 'drupal/legacy-project',
         'dev' => true,
     ),
@@ -665,11 +661,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-<<<<<<< HEAD
-            'reference' => '1f1c7a82d7560bfae581913c2128e90875c2ca5a',
-=======
-            'reference' => '719368494c89a904417b284254a6799927eb685d',
->>>>>>> 6347b75e5 (updat edite mode)
+            'reference' => 'cc89b094e6a9e40fcd253c68a337264e2cbc0b78',
             'dev_requirement' => false,
         ),
         'drupal/link' => array(
