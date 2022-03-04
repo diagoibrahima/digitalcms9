e3604763ab58058ec63fc39e6c454574b60a1dc1
dmgaye96 <doudoumohametgaye@gmail.com>
2022-03-04 10:03:37 +0000

    edite user forme

--- themes/Digital CMS Theme/src/tailwind.js
+++ themes/Digital CMS Theme/src/tailwind.js
@@ -804,19 +804,7 @@ for(i = 0; i < resultdata.length; i++)
      jQuery(submoduletags.toLowerCase()).addClass('supmoduletitle');
      jQuery('.node-content-form input#edit-submit').prop('disabled', false);
       }
-
-      //find all elemtent in the Ckeditor
-      // console.log(description2);
-      /*
-      var res = body.substring(0, 3);
-      if (res == "<h1" || res == "<h2" || res == "<h3" || res == "<h4" || res == "<h5" || res == "<h6") {
-        // activation du button submit
-        jQuery('.node-content-form input#edit-submit').prop('disabled', false);
-      } else {
-        // Desactivation du button submit
-        jQuery('.node-content-form input#edit-submit').prop('disabled', true);
-        jQuery('<div class="messageduformat-texteformat">😬 Oups !  Please review the format of the content.  Heading for Modules  and  Submodules  Normal For messages  </div>').insertBefore('.node-content-form div#edit-actions #edit-submit'); }
-        */
+      
     });
     
   });
