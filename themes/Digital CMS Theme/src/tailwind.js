5db6891fab4dd938cbdde9efbc8d28746ff01a9f
dmgaye96 <doudoumohametgaye@gmail.com>
2022-03-14 17:36:35 +0000

    update

--- themes/Digital CMS Theme/src/tailwind.js
+++ themes/Digital CMS Theme/src/tailwind.js
@@ -500,14 +500,14 @@ jQuery('form#views-exposed-form-list-of-content-page-1').( function(){
 jQuery(document).ready(function () {
 
 
+  jQuery(".button-delete-section").click(function(){
   
-
-
- 
-
-
-
- // jQuery("article .node-content p").append('<i class="fa fa-trash-o button-delete-section" aria-hidden="true"></i>')
+    jQuery(".contextual.simpler_quickedit-do button.trigger.focusable ").click();
+    console.log("click delete")
+    alert("i am ready ")
+  
+  });
+  jQuery("article .node-content p ").append('<i class="fa fa-trash-o button-delete-section" aria-hidden="true"></i>')
   jQuery('.module-title').removeClass('course-content')
 
    jQuery('article .node-content .module-title').append('<svg class="svg-inline--fa fa-chevron-down fa-w-14 expand-minimize-button text-light text-xl font-thin text-gray-400" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>');
