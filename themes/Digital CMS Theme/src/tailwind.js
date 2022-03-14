ab2877d17a4208e085afa5a5ab05d8d3f313f1b7
dmgaye96 <49489900+dmgaye96@users.noreply.github.com>
2022-03-14 12:52:23 +0000

    Posibility to add and delete sections in the cours

--- themes/Digital CMS Theme/src/tailwind.js
+++ themes/Digital CMS Theme/src/tailwind.js
@@ -475,7 +475,7 @@ jQuery(document).ready(function () {
 
 
 
-  jQuery("article .node-content p").append('<div class="contextual simpler_quickedit-do"><button class="trigger focusable" type="button"><i class="fa fa-trash-o" aria-hidden="true"></i></button></div>')
+  jQuery("article .node-content p").append('<div class="contextual simpler_quickedit-do"><button class="trigger focusable" type="button">Edit</button></div><i class="fa fa-trash-o" aria-hidden="true"></i>')
   jQuery('.module-title').removeClass('course-content')
 
    jQuery('article .node-content .module-title').append('<svg class="svg-inline--fa fa-chevron-down fa-w-14 expand-minimize-button text-light text-xl font-thin text-gray-400" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>');
